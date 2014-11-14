35fe3195b91a82f314d8f8cd722e19c1847c42f5
kimikien <ngotrungkienbk56@gmail.com>
2014-11-15 00:52:10 +0700

    Cap nhat Main

--- BTL_JAVA_xpYoutube/src/application/Main.java
+++ BTL_JAVA_xpYoutube/src/application/Main.java
@@ -1,23 +1,30 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage)  {
+		
+			try {
+				Pane root = FXMLLoader.load(getClass().getResource("/layout/Main.fxml"));
+				Scene scene = new Scene(root,600,400);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setTitle("XEM PHIM JAVA");
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 	}
 	
 	public static void main(String[] args) {
