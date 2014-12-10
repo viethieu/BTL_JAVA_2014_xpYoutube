96084b579f77da732c705f02200dff4ed09d1623
kimikien <ngotrungkienbk56@gmail.com>
2014-12-10 21:47:55 +0700

    update controller on

--- BTL_JAVA_xpYoutube/src/controller/Controller_On.java
+++ BTL_JAVA_xpYoutube/src/controller/Controller_On.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -21,8 +23,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import application.ParseJsonYoutube;
 
@@ -61,17 +65,18 @@ public class Controller_On implements Initializable {
 
 	public void launch(String str1, String str2) {
 		link = str2;
-		if (link != null) {			
+		if (link != null) {
 			final WebView webView = idWebView;
-			webView.getEngine()
-					.loadContent(
-							"<iframe width=\"640\" height=\"380\" src=\"http://www.youtube.com/embed/"+link+"\" frameborder=\"0\" allowfullscreen></iframe>");
+			webView.getEngine().loadContent(
+					"<iframe width=\"640\" height=\"380\" src=\"http://www.youtube.com/embed/"
+							+ link
+							+ "\" frameborder=\"0\" allowfullscreen></iframe>");
 		}
 
 		Stage primaryStage = new Stage();
 		primaryStage.setTitle(str1);
 		Image iconSoftWare = new Image("/layout/iconMovie.png");
-		primaryStage.getIcons().add(iconSoftWare);	
+		primaryStage.getIcons().add(iconSoftWare);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		System.out.println(str2);
@@ -106,6 +111,66 @@ public class Controller_On implements Initializable {
 
 				Controller_Search ctrl = new Controller_Search(list);
 				ctrl.launch();
+			}
+		});
+		idMAbout.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				AnchorPane root;
+				try {
+					root = FXMLLoader.load(getClass().getResource(
+							"/layout/Team.fxml"));
+					Scene scene = new Scene(root);
+					Stage primaryStage = new Stage();
+					primaryStage.setTitle("About YourTube");
+					primaryStage.setScene(scene);
+					primaryStage.show();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		idMOpen.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				final FileChooser fileChooser = new FileChooser();
+				Stage stage = new Stage();
+				File file = fileChooser.showOpenDialog(stage);
+				if (file != null) {
+					// System.out.println(file.getAbsolutePath());
+					// openFile(file);
+					// Controller_Main ctrl = new Controller_Main();
+					// try {
+					// Writer
+					// //PrintWriter log_off = new PrintWriter(new
+					// FileWriter("log_offline.txt"));
+					// PrintWriter log_off= new PrintWriter(filename,true);
+					// log_off.println("file:///" + file.getAbsolutePath());
+					// log_off.flush();
+					//
+					// } catch (IOException e) {
+					// // TODO Auto-generated catch block
+					// e.printStackTrace();
+					// }
+
+					// player.stop();
+					try {
+						String filename = "log_offline.txt";
+						FileWriter fw = new FileWriter(filename, true);
+						fw.write(file.getAbsolutePath() + "\n");// appends the
+																// string to the
+																// file
+						fw.close();
+					} catch (IOException ioe) {
+						System.err.println("IOException: " + ioe.getMessage());
+					}
+					Controller_Main ctrl= new Controller_Main();
+					ctrl.launch("Test", "file:///"+file.getAbsolutePath());
+				}
 			}
 		});
 	}
