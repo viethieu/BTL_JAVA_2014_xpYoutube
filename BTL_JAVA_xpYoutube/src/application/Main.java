9be21bc7baadb66d2323b77da19027f52477baf9
DucHieu <DucHieu@DucHieuLaptop>
2014-12-12 09:15:37 +0700

    test thu

--- BTL_JAVA_xpYoutube/src/application/Main.java
+++ BTL_JAVA_xpYoutube/src/application/Main.java
@@ -2,9 +2,11 @@ package application;
 
 import java.io.IOException;
 
+import controller.Controller_Chose;
 import controller.Controller_Main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
@@ -13,10 +15,13 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private Parent pane;
+	private Scene scene;
+	
 	@Override
 	public void start(Stage primaryStage)  {
-		Controller_Main ctrl = new Controller_Main();
-		ctrl.launch("Xem phim youtube", null);
+		Controller_Chose ctrl = new Controller_Chose();
+		ctrl.launch();
 	}
 	
 	public static void main(String[] args) {
